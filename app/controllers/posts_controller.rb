be99ab1525b402913050c3ee047e9ea958109755
shouhei-yamamoto <elefish19920509@gmail.com>
2022-02-03 11:16:43 +0900

    パーシャルの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update]
+  
   def index
     @posts = Post.all
   end
@@ -17,15 +19,12 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to posts_path, notice: "ブログを編集しました！"
     else
@@ -37,5 +36,9 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:content)
-  end  
+  end 
+  
+  def set_blog
+    @post = Post.find(params[:id])
+  end
 end
