7b568ed28208512e95fb2637d15dc329c298b104
shouhei-yamamoto <elefish19920509@gmail.com>
2022-02-03 14:43:38 +0900

    確認画面での戻るボタンの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,10 +11,14 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    if @post.save
-      redirect_to posts_path, notice: "つぶやきを作成しました！"
-    else
+    if params[:back]
       render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: "つぶやきを作成しました！"
+      else
+        render :new
+      end
     end
   end
 
